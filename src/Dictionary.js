5a2a3da744a8d2f9e7493e6200545e57c4b2170e
Christina <98745726+ChristinaFreisleben@users.noreply.github.com>
2022-08-20 16:09:45 +0200

    Added Dictionary component

---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [searchterm, setSearchterm] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for "${searchterm}"...`);
+  }
+
+  function handleSearchtermChange(event) {
+    setSearchterm(event.target.value);
+  }
+
+  return (
+    <form onSubmit={search}>
+      <input
+        type="search"
+        placeholder="Enter a word ..."
+        onChange={handleSearchtermChange}
+      />
+      <input type="submit" />
+    </form>
+  );
+}
